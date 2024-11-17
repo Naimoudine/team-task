4eec03247cce6f4de5367d2abfdfd21486d5bc18
Naïm <naimoudinesaidi@gmail.com>
2024-11-17 01:02:46 +0100

    footer responsive

--- client/src/components/landing/Footer.tsx
+++ client/src/components/landing/Footer.tsx
@@ -4,37 +4,39 @@ type Props = {};
 
 export default function Footer({}: Props) {
   return (
-    <footer className="w-full flex gap-16 items-start justify-between py-20 mx-auto border-t border-zinc-200 mt-32 px-[12.5rem]">
+    <footer className="w-full flex flex-col gap-16 items-start py-10 mx-auto border-t border-zinc-200 mt-32 xl:px-[10.5rem]">
       <p id="logo">Team-task</p>
-      <ul className="flex flex-col gap-4">
-        <li className="font-medium">Product</li>
-        <li>
-          <Link className="text-black/70 hover:text-black" to="">
-            Princing
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex flex-col gap-4">
-        <li className="font-medium">Company</li>
-        <li>
-          <Link className="text-black/70 hover:text-black" to="/contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-      <ul className="flex flex-col gap-4">
-        <li className="font-medium">Legal</li>
-        <li>
-          <Link className="text-black/70 hover:text-black" to="">
-            Terms
-          </Link>
-        </li>
-        <li>
-          <Link className="text-black/70 hover:text-black" to="">
-            Privacy
-          </Link>
-        </li>
-      </ul>
+      <div className="flex justify-between w-full">
+        <ul className="flex flex-col gap-4">
+          <li className="font-medium">Product</li>
+          <li>
+            <Link className="text-black/70 hover:text-black" to="">
+              Princing
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex flex-col gap-4">
+          <li className="font-medium">Company</li>
+          <li>
+            <Link className="text-black/70 hover:text-black" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <ul className="flex flex-col gap-4">
+          <li className="font-medium">Legal</li>
+          <li>
+            <Link className="text-black/70 hover:text-black" to="">
+              Terms
+            </Link>
+          </li>
+          <li>
+            <Link className="text-black/70 hover:text-black" to="">
+              Privacy
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
